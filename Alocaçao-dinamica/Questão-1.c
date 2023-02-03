7f465518af7e11bbb6672a9d7477bef560d2136a
Luís F. Tomaz <116278191+lftmaz@users.noreply.github.com>
2023-02-03 10:15:25 -0300

    Create Questão-1.c

--- Alocaçao-dinamica/Questão-1.c
+++ Alocaçao-dinamica/Questão-1.c
@@ -1,1 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+int main(void){
+    int tamanho = 10,i;
+    int * vetor;
+    vetor = (int*) malloc(tamanho * sizeof(int));
+    if (vetor == NULL)
+    {
+        printf("Erro!");
+        exit(1);
+
+    }
+
+    for (i = 0; i < tamanho; i++)
+    {
+        vetor[i] = i;
+    }
+
+    for (i = 0; i < tamanho; i++)
+    {
+       
+        printf("%i \t", vetor[i]);
+
+    }
+
+    free(vetor);
+
+    return(0);
+}
