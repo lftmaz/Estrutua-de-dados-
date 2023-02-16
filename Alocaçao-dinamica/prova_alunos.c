b9f3a6f13be08a42eda959bfb6c2085546679247
Luís F. Tomaz <116278191+lftmaz@users.noreply.github.com>
2023-02-16 10:45:23 -0300

    Update prova_alunos.c

--- Alocaçao-dinamica/prova_alunos.c
+++ Alocaçao-dinamica/prova_alunos.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
 
 int main(void)
 {
-    int numero_questoes = 0, i, j, acertos, aprovado = 0;
+    int numero_questoes = 0, i, j, acertos = 0, aprovado = 0;
     float porcetagem_aprovacao = 0, vetor_alunos[10], valor_questoes = 0;
 
     printf("Quantas questoes possui a prova?");
     scanf("%d", &numero_questoes);
 
-    valor_questoes = 10/numero_questoes;
+    valor_questoes = (float)10 / numero_questoes;
 
-    char *gabarito = (char *) malloc(numero_questoes * sizeof(char));
+    char *gabarito = (char *)malloc(numero_questoes * sizeof(char));
     if (gabarito == NULL)
     {
         exit(1);
@@ -21,7 +20,7 @@ int main(void)
     printf("Digite o gabarito da prova:\n");
     for (i = 0; i < numero_questoes; i++)
     {
-        scanf(" %s" , &gabarito[i]);
+        scanf(" %s", &gabarito[i]);
     }
 
     char **aluno = (char **)malloc(10 * sizeof(char *));
@@ -32,7 +31,7 @@ int main(void)
 
     for (i = 0; i < 10; i++)
     {
-        printf("Digite as respostas do %d aluno: \n" , i + 1);
+        printf("Digite as respostas do %d aluno: \n", i + 1);
         for (j = 0; j < numero_questoes; j++)
         {
             scanf(" %s", &aluno[i][j]);
@@ -48,29 +47,29 @@ int main(void)
                 acertos++;
             }
         }
-        vetor_alunos[i] = 0;
-        vetor_alunos[i] = acertos * valor_questoes;
+        vetor_alunos[i] = valor_questoes * acertos;
         acertos = 0;
 
-        printf("Nota do %i aluno: %.2f \n" , i +1 , vetor_alunos[i]);
+        printf("Nota do %i aluno: %.2f \n", i + 1, vetor_alunos[i]);
     }
 
-    for(i = 0; i < 10; i++){
-        if(vetor_alunos[i] >= 6.0){
+    for (i = 0; i < 10; i++)
+    {
+        if (vetor_alunos[i] >= 6.0)
+        {
             aprovado++;
         }
-        
     }
 
-    porcetagem_aprovacao = (aprovado/10) * 100;
+    porcetagem_aprovacao = (aprovado * 100)/10;
 
-    printf("A taxa de aprovação é: %.2f \n" , porcetagem_aprovacao);
+    printf("A taxa de aprovação é: %.2f \n", porcetagem_aprovacao);
 
     for (i = 0; i < 10; i++)
     {
         free(aluno[i]);
     }
     free(aluno);
-    
+
     return (0);
 }
