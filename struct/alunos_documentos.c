7dfbd39c8b79b87641b9e2938e18e839350b76b7
Luís F. Tomaz <116278191+lftmaz@users.noreply.github.com>
2023-02-15 22:44:51 -0300

    Add files via upload

---
+++ struct/alunos_documentos.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+
+typedef union documentos
+{
+    int cpf;
+    int sus;
+    int rg;
+}Documentos;
+
+typedef struct aluno
+{
+    char nome[50];
+    int matricula;
+    Documentos doc;
+}Aluno;
+
+void imprimir(Aluno aluno , int caso){
+    printf("Nome do aluno: %s\n" , aluno.nome);
+    printf("Sua matricula: %d \n" , aluno.matricula);
+    if( caso == 1){
+    printf("Seu documento: %d" , aluno.doc.cpf);
+    }
+    else if( caso == 2){
+    printf("Seu documento: %d" , aluno.doc.rg);
+    }
+    else{
+    printf("Seu documento: %d" , aluno.doc.sus);
+    }
+}
+
+int main(void)
+{
+    Aluno *aluno;
+    int opcao;
+    printf("Informe o nome do aluno\n");
+    scanf(" %[^\n]s" , aluno->nome);
+    printf("Informe a matricula:\n");
+    scanf("%d", &aluno->matricula);
+
+    printf("Informe o documento\n 1 - CPF\n 2 - RG\n 3 - SUS \nSua escolha:");
+    scanf("%d" , (int*) &opcao);
+
+    switch (opcao)
+    {
+    case 1:
+        printf("Informe o CPF:\n");
+        scanf("%d" , &aluno->doc.cpf);
+        imprimir(*aluno,1);
+        break;
+    
+    case 2:
+        printf("Informe o RG:\n");
+        scanf("%d" , &aluno->doc.rg);
+        imprimir(*aluno,2);
+        break;
+    
+    case 3:
+        printf("Informe o SUS:\n");
+        scanf("%d" , &aluno->doc.sus);
+        imprimir(*aluno,3);
+        break;
+    
+    default:
+        printf("Opcao Invalida!\n");
+        break;
+    }
+
+    
+
+    return(0);
+}
