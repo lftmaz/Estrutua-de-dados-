b5eee618ee1cc253fbff17c03b9e4bec8f7611a1
Luís F. Tomaz <116278191+lftmaz@users.noreply.github.com>
2023-02-25 00:34:51 -0300

    Update Hexagono.c

--- Ponteiros/Hexagono.c
+++ Ponteiros/Hexagono.c
@@ -4,15 +4,15 @@
 
 void calcula_hexagono(float lado,float *area,float *perimetro){
     *perimetro = 6*lado;
-    *area = ((3 * pow(lado,2))*sqrt(3))/2;
+    *area = ((3 * pow(lado,2))*sqrt(3))/2; // Calculando o perimetro e a area
 }
 
 int main(void){
-    float lado, area,perimetro;
+    float lado, area,perimetro; //Declarando variaveis
     printf("Digite o tamanho do lado do hexagono:\n");
     scanf("%f" , &lado);
-    calcula_hexagono(lado,&area,&perimetro);
+    calcula_hexagono(lado,&area,&perimetro); //Passando parâmetro
     printf("%f\n" , perimetro);
-    printf("%f\n" , area);
+    printf("%f\n" , area); // Exibindo perimetro e area
     return(0);
 }
