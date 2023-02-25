e3a6a8db20943c745c31645c158be06cae74561b
Luís F. Tomaz <116278191+lftmaz@users.noreply.github.com>
2023-02-25 00:59:37 -0300

    Update alunos.c

--- struct/alunos.c
+++ struct/alunos.c
@@ -2,16 +2,16 @@
 #include<stdlib.h>
 
 
-typedef struct aluno{
+typedef struct aluno{ // Criando a struct alunos
     char nome[50];
     int matricula;
     float IRA;
 }Aluno;
 
-void imprimir(Aluno *impressao, int nmr_alunos){
+void imprimir(Aluno *impressao, int nmr_alunos){ // Imprimindo alunos
 
     int cont;
-     for(cont = 0; cont < nmr_alunos; cont++){
+     for(cont = 0; cont < nmr_alunos; cont++){ // Imprimindo os dados da quantidade de alunos que possui
         printf("O nome do %i aluno é: %s\n" , cont + 1, impressao[cont].nome);
         printf("A matricula do %iº é: %d\n" , cont + 1, impressao[cont].matricula);
         printf("O IRA do %iº é :%f\n" , cont + 1, impressao[cont].IRA);
@@ -19,17 +19,17 @@ void imprimir(Aluno *impressao, int nmr_alunos){
 }
 
 int main(void){
-    Aluno *vetor_aluno;
+    Aluno *vetor_aluno; // Declarando variaveis
     int qntd_alunos,cont;
     printf("Digite o numero de alunos que voce deseja cadastrar:\n");
     scanf("%i", &qntd_alunos);
-    vetor_aluno = (Aluno*) malloc(qntd_alunos * sizeof(Aluno));
+    vetor_aluno = (Aluno*) malloc(qntd_alunos * sizeof(Aluno)); // Alocando o vetor_alunos
     if (vetor_aluno == NULL)
     {
         exit(1);
     }
     
-    for(cont = 0; cont < qntd_alunos; cont++){
+    for(cont = 0; cont < qntd_alunos; cont++){ // Preechendo o vetor_alunos com os dados que a struct possui
         printf("Digite o nome do %iº do aluno:\n " , cont + 1);
         scanf(" %[^\n]s" , vetor_aluno[cont].nome);
         printf("Digite a matricula do %iº: \n", cont + 1);
@@ -38,9 +38,9 @@ int main(void){
         scanf("%f", &vetor_aluno[cont].IRA);
     }
 
-    imprimir(vetor_aluno, qntd_alunos);
+    imprimir(vetor_aluno, qntd_alunos); /// Passando os parametros da função
 
-    free(vetor_aluno);
+    free(vetor_aluno); // Desalocando a memoria
     
     return(0);
 }
