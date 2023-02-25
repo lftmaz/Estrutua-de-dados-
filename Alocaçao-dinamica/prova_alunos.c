20a647324f99f6a290e0c4bbac1d505ce92bedfc
Luís F. Tomaz <116278191+lftmaz@users.noreply.github.com>
2023-02-25 00:45:09 -0300

    Update prova_alunos.c

--- Alocaçao-dinamica/prova_alunos.c
+++ Alocaçao-dinamica/prova_alunos.c
@@ -3,27 +3,27 @@
 
 int main(void)
 {
-    int numero_questoes = 0, i, j, acertos = 0, aprovado = 0;
+    int numero_questoes = 0, i, j, acertos = 0, aprovado = 0; // Declarando variaveis
     float porcetagem_aprovacao = 0, vetor_alunos[10], valor_questoes = 0;
 
     printf("Quantas questoes possui a prova?");
     scanf("%d", &numero_questoes);
 
-    valor_questoes = (float)10 / numero_questoes;
+    valor_questoes = (float)10 / numero_questoes; // Calculando quanto cada questao vale
 
-    char *gabarito = (char *)malloc(numero_questoes * sizeof(char));
+    char *gabarito = (char *)malloc(numero_questoes * sizeof(char)); // Alocando a matriz gabarito dinamicamente
     if (gabarito == NULL)
     {
         exit(1);
     }
 
-    printf("Digite o gabarito da prova:\n");
+    printf("Digite o gabarito da prova:\n"); // Obtendo o gabarito
     for (i = 0; i < numero_questoes; i++)
     {
         scanf(" %s", &gabarito[i]);
     }
 
-    char **aluno = (char **)malloc(10 * sizeof(char *));
+    char **aluno = (char **)malloc(10 * sizeof(char *)); // Alocando a matriz dos alunos e respostas
     for (i = 0; i < 10; i++)
     {
         aluno[i] = (char *)malloc(numero_questoes * sizeof(char));
@@ -31,7 +31,7 @@ int main(void)
 
     for (i = 0; i < 10; i++)
     {
-        printf("Digite as respostas do %d aluno: \n", i + 1);
+        printf("Digite as respostas do %d aluno: \n", i + 1); // Obtendo a respotas de cada aluno
         for (j = 0; j < numero_questoes; j++)
         {
             scanf(" %s", &aluno[i][j]);
@@ -42,30 +42,30 @@ int main(void)
     {
         for (j = 0; j < numero_questoes; j++)
         {
-            if (aluno[i][j] == gabarito[j])
+            if (aluno[i][j] == gabarito[j]) // Somando os acertos 
             {
                 acertos++;
             }
         }
-        vetor_alunos[i] = valor_questoes * acertos;
+        vetor_alunos[i] = valor_questoes * acertos; // Preenchendo a nota de cada aluno
         acertos = 0;
 
-        printf("Nota do %i aluno: %.2f \n", i + 1, vetor_alunos[i]);
+        printf("Nota do %i aluno: %.2f \n", i + 1, vetor_alunos[i]); // Exibindo a nota de cada aluno
     }
 
     for (i = 0; i < 10; i++)
     {
-        if (vetor_alunos[i] >= 6.0)
+        if (vetor_alunos[i] >= 6.0) // Calculando quantos foram aprovados
         {
             aprovado++;
         }
     }
 
-    porcetagem_aprovacao = (aprovado * 100)/10;
+    porcetagem_aprovacao = (aprovado * 100)/10; // Calculando a porcetagem de aprovação
 
-    printf("A taxa de aprovação é: %.2f \n", porcetagem_aprovacao);
+    printf("A taxa de aprovação é: %.2f \n", porcetagem_aprovacao); 
 
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < 10; i++) // Desalocando a memoria 
     {
         free(aluno[i]);
     }
